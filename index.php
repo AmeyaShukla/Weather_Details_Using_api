55a8d34385caf51653cf4e206d294ce024ca1299
Ameya Shukla <ameya.shukla5@gmail.com>
2015-11-28 21:02:55 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<meta>
+    <title>MY WEB</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel='stylesheet' href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/index_css.css">
+    <script src="js/index_javascript.js"></script>
+
+
+
+
+
+</head>
+<body bgcolor="#7fffd4" onload="initial_functions()">
+<div id="main" class="container-fluid">
+// the weather detials which are recieved from the api will be displayed here...
+    <div id="show_data" >
+
+
+
+</div>
+
+</div>
+
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ameya
+ * Date: 11/27/2015
+ * Time: 8:51 AM
+ */
+?>
+
